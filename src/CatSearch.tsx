e706564eca0c37166bd9a0de4e50b167c3010c48
Lunnatica <paulalopezantelo@gmail.com>
2024-08-12 21:50:30 +0100

    feat: proposed changes

--- src/CatSearch.tsx
+++ src/CatSearch.tsx
@@ -1,43 +1,82 @@
 import * as React from "react"
 import { useState, useEffect, useCallback } from "react"
 import { fetchSearchResultsFromAPI } from "./searchApiClient"
+import { Cat } from "./types"
+
+const CatItem: React.FC<{ cat: Cat }> = ({ cat }: { cat: Cat }) => {
+  return (
+    <li>
+      <h2>🐈 {cat.breed}</h2>
+      {cat.country && <div>🌍 {cat.country}</div>}
+      {cat.origin && <div>🏠 {cat.origin}</div>}
+      {cat["body type"] && <div>🦴 {cat["body type"]}</div>}
+      {cat.coat && <div>🧥 {cat.coat}</div>}
+      {cat.pattern && <div>🎨 {cat.pattern}</div>}
+    </li>
+  )
+}
+
+const SearchError: React.FC = () => (
+  <div>Error when searching, please try again...</div>
+)
+
+const CatList: React.FC<{ cats: Cat[] }> = ({ cats }: { cats: Cat[] }) => {
+  if (cats.length === 0) {
+    return <div>No search results</div>
+  }
+
+  return (
+    <ul>
+      {cats.map((cat) => (
+        <CatItem key={cat.breed} cat={cat} />
+      ))}
+    </ul>
+  )
+}
 
 export function CatSearch() {
+  const [hasErrored, setHasErrored] = useState(false)
   const [valid, setValid] = useState(false)
-  const [hits, setHits] = useState<any[]>([])
+  const [searchResults, setSearchResults] = useState<Cat[]>([])
   const [searchTerm, setSearchTerm] = useState("")
 
-  const searchTermOnChange = useCallback((event) => {
-    const { value } = event.target
-    setSearchTerm(value)
-  }, [])
+  const updateSearchTerm = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const handleSearchTermChange = useCallback(updateSearchTerm, [])
 
   useEffect(() => {
-    setValid(true)
+    const fetchSearchResults = async () => {
+      try {
+        const results = await fetchSearchResultsFromAPI(searchTerm)
+        setSearchResults(results)
+      } catch (error) {
+        setHasErrored(true)
+      }
+    }
+
+    setValid(searchTerm.length >= 2)
+
     if (searchTerm.length < 2) {
-      setValid(false)
+      setSearchResults([])
       return
     }
 
-    fetchSearchResultsFromAPI(searchTerm).then((hits) => {
-      setHits(hits)
-    })
-  })
+    fetchSearchResults()
+  }, [searchTerm])
 
   return (
     <div>
       <h2>Search for cat breed</h2>
-      <input type="text" onChange={searchTermOnChange} />
-      <div>
+      <input type="text" onChange={handleSearchTermChange} id="searchTerm" />
+      <label htmlFor="searchTerm">
         {valid === false
           ? "Type at least two characters to start searching"
           : `You searched for ${searchTerm}`}
-      </div>
-      <div>
-        {(hits && hits.length) > 0
-          ? hits.map((hit, i) => <div key={i}>🐈 {hit.breed}</div>)
-          : "No hits"}
-      </div>
+      </label>
+
+      {hasErrored ? <SearchError /> : <CatList cats={searchResults} />}
     </div>
   )
 }
--- src/searchApiClient.ts
+++ src/searchApiClient.ts
@@ -1,7 +1,25 @@
-export function fetchSearchResultsFromAPI(searchTerm) {
+import { Cat } from "./types"
+
+export async function fetchSearchResultsFromAPI(
+  searchTerm: string
+): Promise<Cat[]> {
   // Note: this API seems a bit unstable and sometimes it takes a long time to respond, especially
   // for broad searches
-  return fetch(
-    `https://cat-api-bjoerge.sanity-io1.now.sh/cats?query=${searchTerm}`
-  ).then((res) => res.json())
+  try {
+    const response = await fetch(
+      `https://cat-api-bjoerge.sanity-io1.now.sh/cats?query=${encodeURIComponent(
+        searchTerm
+      )}`
+    )
+
+    if (!response.ok) {
+      console.error("Error fetching search results:", response.statusText)
+      throw new Error("Failed to fetch search results")
+    }
+
+    return response.json()
+  } catch (error) {
+    console.error("Error fetching search results:", error)
+    throw new Error("Failed to fetch search results")
+  }
 }
